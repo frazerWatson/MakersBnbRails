04c10fe45b9c5e7c7c64a9f2a37e75172eb14d8f
Tom Pickard <tcpickard94@gmail.com>
2016-03-15 16:08:45 +0000

    Completed User Story 3 - User Authentication
    Added link to Sign In in layout
    Updated devise links to user Sign In as apose to Log In
    Wrote a feature test for signing in

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,6 +8,16 @@ feature 'User managment: ' do
     create_user
     click_link "Sign Out"
     expect(page).not_to have_content "John Smith"
-    expect(page).to have_content ""
+    expect(page).to have_content "Signed out successfully."
   end
+
+  scenario 'Sign In' do
+    create_user
+    click_link "Sign Out"
+    click_link "Sign In"
+    fill_in "Email", with: "test@ymail.com"
+    fill_in "Password", with: "123secret"
+    click_button "Sign In"
+    expect(page).to have_content "John Smith"
+  end 
 end
