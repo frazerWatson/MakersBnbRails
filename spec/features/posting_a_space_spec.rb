6863ecd6de3013233a58607dcd75df4e2e0fc838
Reiss Johnson <reissjohnson@me.com>
2016-03-15 11:56:11 +0000

    second test added, user story 2 wip

--- spec/features/posting_a_space_spec.rb
+++ spec/features/posting_a_space_spec.rb
@@ -6,4 +6,15 @@ feature 'a user can post a space' do
      expect(page).to have_link 'post a space'
    end
   end
+
+   context 'spaces have been added' do
+     before do 
+       Space.create(name: 'my hotel')
+     end
+   scenario 'page shows spaces' do
+     visit '/'
+     expect(page).to have_content 'my hotel'
+     expect(page).not_to have_content 'no spaces yet ...'
+   end
+  end
 end
