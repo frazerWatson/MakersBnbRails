52ca6a514001a3e74728e2f930c08a02455d141e
Reiss Johnson <reissjohnson@me.com>
2016-03-15 12:30:32 +0000

    failing test for form, WIP user story 2'

--- spec/features/posting_a_space_spec.rb
+++ spec/features/posting_a_space_spec.rb
@@ -11,10 +11,28 @@ feature 'a user can post a space' do
     before do 
       Space.create(name: 'my hotel')
     end
+
     scenario 'page shows spaces' do
       visit '/'
       expect(page).to have_content 'my hotel'
       expect(page).not_to have_content 'no spaces yet...'
+    end
+  end
+
+  context 'users are able to add a space' do
+    scenario 'adding a space' do
+      visit '/'
+      click_link 'post a space'
+      fill_in 'Name', with: 'my hotel'
+      fill_in 'Location', with: 'London'
+      fill_in 'Price', with: '100'
+      fill_in 'Details', with: 'single bedroom with ensuite'
+      fill_in 'Available from', with: Date.new(2016,03,15)
+      fill_in 'Available to', with: Date.new(2016,03,15)
+      click_button 'Submit'
+      within 'ul#spaces' do
+        expect(page).to have_content 'Location: 100'
+      end
     end
   end
 end
